fde9ff68db2feee7af3773bd44d84d3cf7804085
afernandezrios <andresfdezrios@gmail.com>
2025-12-23 09:16:02 +0100

    Refactot blob download functionality (rearrange code)

---
+++ src/internal/docker/bolbs.go
@@ -0,0 +1,80 @@
+package docker
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type BlobInfo struct {
+	MediaType string `json:"mediaType"`
+	Digest    string `json:"digest"`
+	Size      int    `json:"size"`
+}
+
+// For simplicity, all documents will be downloaded in a hardcoded path and cache headers are ignored
+// Doc: https://distribution.github.io/distribution/spec/api/#pulling-a-layer
+func (c *Client) DownloadBlob(digest string, blobPath string, imageName string) (string, error) {
+
+	blobRequestPath := fmt.Sprintf("https://registry.hub.docker.com/v2/%s/blobs/%s", imageName, digest)
+	req, _ := http.NewRequest("GET", blobRequestPath, nil)
+
+	if c.token != "" {
+		req.Header.Set("Authorization", "Bearer "+c.token)
+	}
+
+	return c.makeGetBlobRequest(req, blobPath)
+}
+
+func (c *Client) makeGetBlobRequest(req *http.Request, blobPath string) (string, error) {
+	resp, err := c.client.Do(req)
+
+	if err != nil {
+		return "", fmt.Errorf("cannot download blob: %s", err)
+	}
+
+	switch resp.StatusCode {
+	case 200:
+		return c.extractBlob(resp, blobPath)
+	default:
+		return "", fmt.Errorf("blob not found: %s", resp.Status)
+	}
+}
+
+func (*Client) extractBlob(resp *http.Response, blobPath string) (string, error) {
+	defer resp.Body.Close()
+
+	out, err := os.Create(blobPath)
+	
+	if err != nil {
+		return "", fmt.Errorf("cannot create blob file: %s", err)
+	}
+
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+
+	if err != nil {
+		return "", fmt.Errorf("cannot copy blob in path '%s': %v", blobPath, err)
+	}
+
+	return blobPath, nil
+}
+
+// Getting extension of the file according to: https://distribution.github.io/distribution/spec/manifest-v2-2/#media-types
+// It also supports OCI standard -> https://github.com/opencontainers/image-spec/blob/main/manifest.md#image-manifest-property-descriptions
+func (blob *BlobInfo) GetExtension() string {
+	switch blob.MediaType {
+	case "application/vnd.docker.image.rootfs.diff.tar.gzip", "application/vnd.oci.image.layer.v1.tar+gzip":
+		// “Layer”, as a gzipped tar
+		return ".tar.gz"
+	case "application/vnd.docker.container.image.v1+json", "application/vnd.oci.image.config.v1+json":
+		// Container config JSON
+		return ".json"
+	default:
+		log.Println("MediaType not implemented: " + blob.MediaType)
+		return ""
+	}
+}
--- src/internal/docker/layers.go
+++ src/internal/docker/layers.go
@@ -18,16 +18,10 @@ type ManifestLayersData struct {
 	Layers        []BlobInfo `json:"layers"`
 }
 
-type BlobInfo struct {
-	MediaType string `json:"mediaType"`
-	Digest    string `json:"digest"`
-	Size      int    `json:"size"`
-}
-
 func (c *Client) DownloadLayers(manifest Manifest, downloadPath string, imageName string) (string, error) {
 
 	layerPath := fmt.Sprintf("https://registry.hub.docker.com/v2/%s/manifests/%s", imageName, manifest.Digest)
-	
+
 	req, _ := http.NewRequest("GET", layerPath, nil)
 
 	if c.token != "" {
@@ -71,135 +65,113 @@ func (c *Client) extractLayers(resp *http.Response, downloadPath string, imageNa
 	}
 
 	c.downloadLayers(layersData, downloadPath, imageName)
-	c.downloadConfig(layersData, downloadPath, imageName)
 
-	return downloadPath, nil
+	if err := c.downloadLayers(layersData, downloadPath, imageName); err != nil {
+		return "", err
+	}
+
+	return c.downloadConfig(layersData, downloadPath, imageName)
 }
 
-func (c *Client) downloadConfig(layersData *ManifestLayersData, downloadPath string, imageName string) {
+func (c *Client) downloadConfig(layersData *ManifestLayersData, downloadPath string, imageName string) (string, error) {
 	log.Printf("Downloading config: %s\n", layersData.Config.Digest)
-	configPath := downloadPath + "config" + getExtension(layersData.Config)
-	c.DownloadBlob(layersData.Config.Digest, configPath, imageName)
+	configPath := downloadPath + "config" + layersData.Config.GetExtension()
+	return c.DownloadBlob(layersData.Config.Digest, configPath, imageName)
 }
 
-func (c *Client) downloadLayers(layersData *ManifestLayersData, downloadPath string, imageName string) {
+func (c *Client) downloadLayers(layersData *ManifestLayersData, downloadPath string, imageName string) error {
+
 	for _, layer := range layersData.Layers {
 		log.Printf("Downloading layer: %s\n", layer.Digest)
-		filePath := downloadPath + layer.Digest + getExtension(layer)
-		c.DownloadBlob(layer.Digest, filePath, imageName)
-		unzipLayer(filePath, downloadPath)
+
+		filePath := downloadPath + layer.Digest + layer.GetExtension()
+
+		if _, err := c.DownloadBlob(layer.Digest, filePath, imageName); err != nil {
+			return err
+		}
+
+		if err := unzipLayer(filePath, downloadPath); err != nil {
+			return err
+		}
 	}
+
+	return nil
 }
 
-// For simplicity, all documents will be downloaded in a hardcoded path and cache headers are ignored
-// Doc: https://distribution.github.io/distribution/spec/api/#pulling-a-layer
-func (c *Client) DownloadBlob(digest string, filePath string, imageName string) string {
+func unzipLayer(zipPath string, destPath string) error {
 
-	out, err := os.Create(filePath)
-	if err != nil {
-		log.Panicf("Error creating layer file: %s", err)
-	}
-	defer out.Close()
-
-	blobPath := fmt.Sprintf("https://registry.hub.docker.com/v2/%s/blobs/%s", imageName, digest)
-	req, _ := http.NewRequest("GET", blobPath, nil)
-	req.Header.Set("Authorization", "Bearer "+c.token)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-
-	if err != nil {
-		log.Panicf("Error downloading layer: %s", err)
-	}
-
-	if resp.StatusCode != 200 {
-		log.Panicf("Layer blob not found: " + resp.Status)
-	}
-
-	defer resp.Body.Close()
-
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		log.Panicf("Cannot copy blob in path '%s': %v", filePath, err)
-	}
-	return filePath
-}
-
-// Getting extension of the file according to: https://distribution.github.io/distribution/spec/manifest-v2-2/#media-types
-// It also supports OCI standard -> https://github.com/opencontainers/image-spec/blob/main/manifest.md#image-manifest-property-descriptions
-func getExtension(blobInfo BlobInfo) string {
-	switch blobInfo.MediaType {
-	case "application/vnd.docker.image.rootfs.diff.tar.gzip", "application/vnd.oci.image.layer.v1.tar+gzip":
-		// “Layer”, as a gzipped tar
-		return ".tar.gz"
-	case "application/vnd.docker.container.image.v1+json", "application/vnd.oci.image.config.v1+json":
-		// Container config JSON
-		return ".json"
-	default:
-		log.Println("MediaType not implemented: " + blobInfo.MediaType)
-		return ""
-	}
-}
-
-func unzipLayer(zipPath string, destPath string) {
-	// Open the gzipped tar file
 	file, err := os.Open(zipPath)
+
 	if err != nil {
-		log.Panicf("Error opening zip layer: %s\n", err)
+		return fmt.Errorf("error opening zip layer: %s", err)
 	}
+
 	defer file.Close()
 
-	// Create a gzip reader
 	gzipReader, err := gzip.NewReader(file)
+
 	if err != nil {
-		log.Panicf("Error creating gzip reader: %s\n", err)
+		return fmt.Errorf("error creating gzip reader: %s", err)
 	}
+
 	defer gzipReader.Close()
 
-	// Create a tar reader
 	tarReader := tar.NewReader(gzipReader)
 
-	// Iterate through the files in the tar archive
 	for {
 		header, err := tarReader.Next()
+
 		if err == io.EOF {
 			break // End of archive
 		}
+
 		if err != nil {
-			log.Panicf("Error iterating files in tar archive: %s\n", err)
+			return fmt.Errorf("error iterating files in tar archive: %s", err)
 		}
 
-		// Handle the file based on its type
-		switch header.Typeflag {
-		case tar.TypeDir:
-			// Create directory
-			if err := os.MkdirAll(destPath+header.Name, os.FileMode(header.Mode)); err != nil {
-				log.Printf("Directory %s cannot be created: %s\n", header.Name, err)
-			}
-		case tar.TypeReg:
-			// Create the file with the specified permissions
-			outFile, err := os.OpenFile(destPath+header.Name, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0777)
-			if err != nil {
-				log.Println("Error creating file:", err)
-			}
-			defer outFile.Close()
+		handleLayerFile(header, destPath, tarReader)
+	}
 
-			// Copy file content
-			if _, err := io.Copy(outFile, tarReader); err != nil {
-				log.Println(err)
-			}
-		case tar.TypeSymlink:
-			// Create symlink
-			if err := os.Symlink(header.Linkname, destPath+header.Name); err != nil {
-				log.Println(err)
-			}
-		case tar.TypeLink:
-			// Create hard link
-			if err := os.Link(destPath+header.Linkname, destPath+header.Name); err != nil {
-				log.Println(err)
-			}
-		default:
-			log.Printf("Unable to handle file type %c\n", header.Typeflag)
+	return nil
+}
+
+func handleLayerFile(header *tar.Header, destPath string, tarReader *tar.Reader) {
+	switch header.Typeflag {
+
+	// Create directory
+	case tar.TypeDir:
+		if err := os.MkdirAll(destPath+header.Name, os.FileMode(header.Mode)); err != nil {
+			log.Printf("Directory %s cannot be created: %s\n", header.Name, err)
 		}
+
+	// Create the file with the specified permissions
+	case tar.TypeReg:
+		outFile, err := os.OpenFile(destPath+header.Name, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0777)
+
+		if err != nil {
+			log.Println("Error creating file:", err)
+		}
+
+		defer outFile.Close()
+
+		// Copy file content
+		if _, err := io.Copy(outFile, tarReader); err != nil {
+			log.Println(err)
+		}
+
+	// Create symlink
+	case tar.TypeSymlink:
+		if err := os.Symlink(header.Linkname, destPath+header.Name); err != nil {
+			log.Println(err)
+		}
+
+	// Create hard link
+	case tar.TypeLink:
+		if err := os.Link(destPath+header.Linkname, destPath+header.Name); err != nil {
+			log.Println(err)
+		}
+
+	default:
+		log.Printf("Unknown file type %c\n", header.Typeflag)
 	}
 }
