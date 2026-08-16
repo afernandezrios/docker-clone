a932ab20985906c8530e691988e19083ffb80c92
afernandezrios <andresfdezrios@gmail.com>
2026-08-16 10:07:42 +0200

    Multiple fixes in layers downloading process

--- src/internal/docker/layers.go
+++ src/internal/docker/layers.go
@@ -22,10 +22,9 @@ type ManifestLayersData struct {
 
 func (c *Client) DownloadLayers(manifest *Manifest, imageName string, downloadDir string) error {
 
-	layerPath := fmt.Sprintf("https://registry.hub.docker.com/v2/%s/manifests/%s", imageName, manifest.Digest)
+	layerPath := fmt.Sprintf("%s/%s/manifests/%s", BaseURL, imageName, manifest.Digest)
 
 	req, _ := http.NewRequest("GET", layerPath, nil)
-
 	if c.token != "" {
 		req.Header.Set("Authorization", "Bearer "+c.token)
 	}
@@ -35,13 +34,13 @@ func (c *Client) DownloadLayers(manifest *Manifest, imageName string, downloadDi
 
 func (c *Client) makeGetLayersRequest(req *http.Request, imageName string, downloadDir string) error {
 	resp, err := c.httpClient.Do(req)
-
 	if err != nil {
 		return fmt.Errorf("cannot download image layers: %v", err)
 	}
+	defer resp.Body.Close()
 
 	switch resp.StatusCode {
-	case 200:
+	case http.StatusOK:
 		return c.extractLayers(resp, imageName, downloadDir)
 	default:
 		return fmt.Errorf("manifest layers not found: %s", resp.Status)
@@ -49,28 +48,25 @@ func (c *Client) makeGetLayersRequest(req *http.Request, imageName string, downl
 }
 
 func (c *Client) extractLayers(response *http.Response, imageName string, downloadDir string) error {
-	defer response.Body.Close()
-
-	layersData := &ManifestLayersData{}
+	var layersData ManifestLayersData
 	if err := json.NewDecoder(response.Body).Decode(&layersData); err != nil {
 		return fmt.Errorf("failed to decode layers response: %v", err)
 	}
 
-	err := os.MkdirAll(downloadDir, 0777)
-	if err != nil {
+	if err := os.MkdirAll(downloadDir, 0755); err != nil {
 		return fmt.Errorf("cannot create directories to store image content: %v", err)
 	}
 
-	if err := c.downloadLayers(layersData, imageName, downloadDir); err != nil {
+	if err := c.downloadLayers(&layersData, imageName, downloadDir); err != nil {
 		return err
 	}
 
-	return c.downloadConfig(layersData, imageName, downloadDir)
+	return c.downloadConfig(&layersData, imageName, downloadDir)
 }
 
 func (c *Client) downloadConfig(layersData *ManifestLayersData, imageName string, downloadDir string) error {
 	log.Printf("Downloading config in %s: %s\n", downloadDir, layersData.Config.Digest)
-	configPath := downloadDir + "/config" + layersData.Config.GetExtension()
+	configPath := filepath.Join(downloadDir, "/config" + layersData.Config.GetExtension())
 	return c.DownloadBlob(layersData.Config.Digest, configPath, imageName)
 }
 
@@ -78,16 +74,18 @@ func (c *Client) downloadLayers(layersData *ManifestLayersData, imageName string
 
 	for _, layer := range layersData.Layers {
 		log.Printf("Downloading layer: %s\n", layer.Digest)
-
-		filePath := downloadDir + layer.Digest + layer.GetExtension()
+		filePath := filepath.Join(downloadDir, layer.Digest+layer.GetExtension())
 
 		if err := c.DownloadBlob(layer.Digest, filePath, imageName); err != nil {
-			return err
+			return fmt.Errorf("download layer blob %s: %w", layer.Digest, err)
 		}
 
 		if err := unzipLayer(filePath, downloadDir); err != nil {
-			return err
+			return fmt.Errorf("unpack layer %s: %w", layer.Digest, err)
 		}
+
+		// Remove tarball after extracting to save disk space
+		_ = os.Remove(filePath)
 	}
 
 	return nil
@@ -96,23 +94,18 @@ func (c *Client) downloadLayers(layersData *ManifestLayersData, imageName string
 func unzipLayer(zipPath string, destPath string) error {
 
 	file, err := os.Open(zipPath)
-
 	if err != nil {
 		return fmt.Errorf("error opening zip layer: %s", err)
 	}
-
 	defer file.Close()
 
 	gzipReader, err := gzip.NewReader(file)
-
 	if err != nil {
 		return fmt.Errorf("error creating gzip reader: %s", err)
 	}
-
 	defer gzipReader.Close()
 
 	tarReader := tar.NewReader(gzipReader)
-
 	for {
 		header, err := tarReader.Next()
 
@@ -124,94 +117,98 @@ func unzipLayer(zipPath string, destPath string) error {
 			return fmt.Errorf("error iterating files in tar archive: %s", err)
 		}
 
-		handleLayerFile(header, destPath, tarReader)
+		if err := handleLayerFile(header, destPath, tarReader); err != nil {
+			return fmt.Errorf("error handling layer file: %s", err)
+		}
 	}
 
 	return nil
 }
 
-func handleLayerFile(header *tar.Header, destPath string, tarReader *tar.Reader) {
-	completeFilePath := header.Name
-	fileName := filepath.Base(completeFilePath)
+func handleLayerFile(header *tar.Header, destPath string, tarReader *tar.Reader) error {
+	// Guard against Zip Slip / path traversal
+	cleanedName := filepath.Clean(header.Name)
+	targetPath := filepath.Join(destPath, cleanedName)
+
+	rel, err := filepath.Rel(destPath, targetPath)
+	if err != nil || strings.HasPrefix(rel, "..") {
+		return fmt.Errorf("path escapes destination: %s", header.Name)
+	}
+
+	baseName := filepath.Base(cleanedName)
 
 	// Documentation about whiteout files: https://specs.opencontainers.org/image-spec/layer/#whiteouts
-	if fileName == ".wh..wh..opq" {
-		handleOpaqueWhiteoutFile(completeFilePath, destPath)
-		return
+	if baseName == ".wh..wh..opq" {
+		return handleOpaqueWhiteoutFile(targetPath)
 	}
-	// Handle regular whiteout
-	if strings.HasPrefix(fileName, ".wh.") {
-		handleRegularWhiteoutFile(fileName, completeFilePath, destPath)
-		return
+	if strings.HasPrefix(baseName, ".wh.") {
+		return handleRegularWhiteoutFile(targetPath, baseName)
 	}
 
-	newFilePath := filepath.Join(destPath, completeFilePath)
-	handleCreationFile(header, newFilePath, tarReader, destPath)
+	newFilePath := filepath.Join(destPath, cleanedName)
+	return handleCreationFile(header, newFilePath, tarReader, destPath)
 }
 
-func handleRegularWhiteoutFile(fileName string, completeFilePath string, destPath string) {
-	log.Printf("Handling regular whiteout: %s\n", fileName)
-	originalFileName := strings.TrimPrefix(fileName, ".wh.")
-	pathToDelete := filepath.Join(destPath, filepath.Dir(completeFilePath), originalFileName)
-	os.RemoveAll(pathToDelete)
+func handleRegularWhiteoutFile(targetPath, baseName string) error {
+	origName := strings.TrimPrefix(baseName, ".wh.")
+	pathToDelete := filepath.Join(filepath.Dir(targetPath), origName)
+	return os.RemoveAll(pathToDelete)
 }
 
-func handleOpaqueWhiteoutFile(completeFilePath string, destPath string) {
-	log.Printf("Handling opaque whiteout: %s\n", completeFilePath)
-	dir := filepath.Join(destPath, filepath.Dir(completeFilePath))
-	entries, _ := os.ReadDir(dir)
+func handleOpaqueWhiteoutFile(targetPath string) error {
+	dir := filepath.Dir(targetPath)
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+
 	for _, e := range entries {
-		os.RemoveAll(filepath.Join(dir, e.Name()))
+		if err := os.RemoveAll(filepath.Join(dir, e.Name())); err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
-func handleCreationFile(header *tar.Header, newFilePath string, tarReader *tar.Reader, destPath string) {
+func handleCreationFile(header *tar.Header, targetPath string, tarReader *tar.Reader, destPath string) error {
 	switch header.Typeflag {
 
 	// Create directory
 	case tar.TypeDir:
-		if err := os.MkdirAll(newFilePath, os.FileMode(header.Mode)); err != nil {
-			log.Printf("Directory %s cannot be created: %s\n", newFilePath, err)
-		}
+		return os.MkdirAll(targetPath, os.FileMode(header.Mode))
 
 	// Create the file with the specified permissions
 	case tar.TypeReg:
-		outFile, err := os.OpenFile(newFilePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0777)
-
+		outFile, err := os.OpenFile(targetPath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0777)
 		if err != nil {
-			log.Println("Error creating file:", err)
+			return err
 		}
-
 		defer outFile.Close()
 
 		// Copy file content
 		if _, err := io.Copy(outFile, tarReader); err != nil {
-			log.Println("Error copying file:", err)
+			return err
 		}
+
+		return nil
 
 	// Create symlink
 	case tar.TypeSymlink:
-		target := header.Linkname
 		// Important to remove existing file/dir/symlink created by previous layers
-		if _, err := os.Lstat(newFilePath); err == nil {
-			if err := os.RemoveAll(newFilePath); err != nil {
-				log.Printf("Error removing existing path %s: %v\n", newFilePath, err)
-				return
-			}
-		}
-
-		if err := os.Symlink(target, newFilePath); err != nil {
-			log.Printf("Error creating symlink %s -> %s: %v\n", newFilePath, target, err)
-		}
+		_ = os.Remove(targetPath)
+		return os.Symlink(header.Linkname, targetPath)
 
 	// Create hard link
 	case tar.TypeLink:
-		newFilePath = filepath.Join(destPath, header.Linkname)
-		if err := os.Link(newFilePath, newFilePath); err != nil {
-			log.Println("Error creating hard link:", err)
-		}
+		oldFilePath := filepath.Join(destPath, header.Linkname)
+		_ = os.Remove(targetPath)
+		return os.Link(oldFilePath, targetPath)
 
 	default:
 		log.Printf("Unknown file type %c\n", header.Typeflag)
+		return nil
 	}
 }
